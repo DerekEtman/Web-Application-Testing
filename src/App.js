3e992cab370f5e6975f4422f66385180bc12986a
Derek Etman <Dereketman@gmail.com>
2019-09-05 18:09:21 -0700

    Saving just in case

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Dashboard from './components/Dashboard';
 
 function App() {
-
   return (
-    <div>  
-      <Dashboard />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/hooks/useIncreaseValue.jsx
+++
@@ -1,3 +0,0 @@
-export const useIncreaseValue = (key, initialValue) => {
-    
-}
